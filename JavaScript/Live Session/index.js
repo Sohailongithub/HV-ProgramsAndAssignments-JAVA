1c4feb1fadfc17bd03d67cc51567414d24da0d39
unknown <sohailshaikh2379@gmail.com>
2023-11-19 17:02:36 +0530

    To DO List APP

--- JavaScript/Live Session/index.js
+++ JavaScript/Live Session/index.js
@@ -50,7 +50,6 @@ mydiv.addEventListener('click', function () {
 
 }) 
 // btn should be outside of whole blue box ;
-box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 
 let boxShadow = document.querySelector('.boxShadow');
 boxShadow.addEventListener('boxShadow', function(){
