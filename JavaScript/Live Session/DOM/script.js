e2770234401035867358cc58be225fbadbd947cb
unknown <sohailshaikh2379@gmail.com>
2023-12-20 18:52:14 +0530

    DOM

---
+++ JavaScript/Live Session/DOM/script.js
@@ -0,0 +1,10 @@
+// 4 pillars of DOM 
+// Selection of an element 
+// Change in  HTML 
+// Change in CSS
+// Add Event Listner  
+
+const a = document.querySelector("h1");
+a.innerHTML = "Change DOM is Working"
+a.style.color= "blue";
+
